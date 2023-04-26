94f93aa448cab38f010b63fa2a1d91bdb2e282d9
kaice88 <chinguyen2k1@gmail.com>
2023-04-26 23:33:02 +0700

    best.pt train7

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 
 
 # load a pretrained model (recommended for training)
-model = YOLO('best.pt')
+model = YOLO('D:/YOLO/runs/pose/train7/weights/best.pt')
 
 
 # Train the model
