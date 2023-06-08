dac016236e45f68a574e667659cee1ef218f9368
kaice88 <chinguyen2k1@gmail.com>
2023-06-08 22:21:18 +0700

    Draw pose loss plot

--- predict.py
+++ predict.py
@@ -2,31 +2,27 @@ from ultralytics import YOLO
 import os
 
 
-model = YOLO('D:/YOLO/runs/pose/train11/weights/best.pt')
-# model = YOLO('D:/YOLO/best.pt')
-# model = YOLO('yolov8n-pose.pt') 
-# # Specify the path to the parent directory containing the subfolders
+model = YOLO('D:/YOLO/best.pt')
+
+# Specify the path to the parent directory containing the subfolders
 # parent_dir = 'C:/Users/Lenovo/Desktop/Processed'
-# folder_name = ['Wrong_Head', 'Correct_Head']
-# # # Create an empty list to store the file paths
-# file_paths = []
+folder_name = ['Wrong_Head', 'Correct_Head']
+#  Create an empty list to store the file paths
+file_paths = []
 
-# # Loop through each subfolder specified in the arr list
-# for subfolder in folder_name:
-#     # Construct the full path to the subfolder
-#     subfolder_path = os.path.join(parent_dir, subfolder)
-#     # Loop through each file in the subfolder
-#     for filename in os.listdir(subfolder_path):
-#         # Check if the file is a .jpg file
-#         if filename.endswith('.jpg'):
-#             # Construct the full path to the file and add it to the list
-#             file_path = os.path.join(subfolder_path, filename)
-#             file_paths.append(file_path)
+# Loop through each subfolder specified in the arr list
+for subfolder in folder_name:
+    # Construct the full path to the subfolder
+    subfolder_path = os.path.join(parent_dir, subfolder)
+    # Loop through each file in the subfolder
+    for filename in os.listdir(subfolder_path):
+        # Check if the file is a .jpg file
+        if filename.endswith('.jpg'):
+            # Construct the full path to the file and add it to the list
+            file_path = os.path.join(subfolder_path, filename)
+            file_paths.append(file_path)
 
 
 
-# for i in file_paths:
-#     model.predict(i, save=True, save_txt=True, conf=0.01)
-
-model.predict("C:/Users/Lenovo/Downloads/Processed/train/0006_jpg.rf.fd913997beef0aac930d5add51d6516c.jpg", save=True, save_txt=True, conf=0.1)
-
+for i in file_paths:
+    model.predict(i, save=True, save_txt=True, conf=0.01)
